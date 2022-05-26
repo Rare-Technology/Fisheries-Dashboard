d0e30bbe1b830ab6fb335e2f0983519aa4feec4f
ibidyouadu <angel.d.umana@gmail.com>
2022-05-25 20:38:08 -0400

    some plot layout changes

--- app.py
+++ app.py
@@ -217,22 +217,54 @@ def update_plot(n_clicks, sel_maa, start_date, end_date):
         ste_catch_value_usd = ('total_price_usd', 'sem')
     ).reset_index()
 
-    catch_fig = px.bar(catch_data, x = 'yearmonth', y = 'weight_mt')
-    catch_fig.update_layout(
-        title_text = "Total Catch per Month (metric tons)"
+    catch_fig = px.bar(
+        catch_data,
+        x = 'yearmonth', y = 'weight_mt',
+        labels = {
+            'yearmonth': '',
+            'weight_mt': ''
+        },
+        title = "Total Catch per Month (metric tons)"
     )
 
     cpue_value_fig = make_subplots(specs = [[{'secondary_y': True}]])
     cpue_value_fig.add_trace(
-        go.Scatter(x = cpue_value_data['yearmonth'], y = cpue_value_data['avg_cpue_kg_trip']),
-        secondary_y = False
+        go.Scatter(
+            x = cpue_value_data['yearmonth'],
+            y = cpue_value_data['avg_cpue_kg_trip'],
+            name = 'CPUE',
+            marker_color = '#5cb9ea'
+        ), secondary_y = False
     )
     cpue_value_fig.add_trace(
-        go.Scatter(x = cpue_value_data['yearmonth'], y = cpue_value_data['avg_catch_value_usd']),
-        secondary_y = True
+        go.Scatter(
+            x = cpue_value_data['yearmonth'],
+            y = cpue_value_data['avg_catch_value_usd'],
+            name = 'Catch Value',
+            marker_color = '#f47762'
+        ), secondary_y = True
     )
     cpue_value_fig.update_layout(
-        title_text = 'Average CPUE and Catch Value'
+        title = 'Average CPUE per Trip and Average Catch Value',
+        xaxis_title = '',
+        yaxis = {
+            'title': 'CPUE (kg/trip)',
+            'titlefont': {
+                'color': '#5cb9ea'
+            },
+            'tickfont': {
+                'color': '#5cb9ea'
+            }
+        },
+        yaxis2 = {
+            'title': 'Catch Value (USD)',
+            'titlefont': {
+                'color': '#f47762'
+            },
+            'tickfont': {
+                'color': '#f47762'
+            }
+        }
     )
 
     return catch_fig, cpue_value_fig
--- mod_plot.py
+++ mod_plot.py
@@ -51,33 +51,66 @@ cpue_value_data = all_data.query(
     ste_catch_value_usd = ('total_price_usd', 'sem')
 ).reset_index()
 
-catch_fig = px.bar(catch_data, x = 'yearmonth', y = 'weight_mt')
+catch_fig = px.bar(
+    catch_data,
+    x = 'yearmonth', y = 'weight_mt',
+    labels = {
+        'yearmonth': '',
+        'weight_mt': ''
+    },
+    title = "Total Catch per Month (metric tons)",
+)
 catch_plot = dcc.Graph(id = 'catches-plot', figure = catch_fig)
 
 cpue_value_fig = make_subplots(specs = [[{'secondary_y': True}]])
 cpue_value_fig.add_trace(
-    go.Scatter(x = cpue_value_data['yearmonth'], y = cpue_value_data['avg_cpue_kg_trip']),
-    secondary_y = False
+    go.Scatter(
+        x = cpue_value_data['yearmonth'],
+        y = cpue_value_data['avg_cpue_kg_trip'],
+        name = 'CPUE',
+        marker_color = '#5cb9ea'
+    ), secondary_y = False
 )
 cpue_value_fig.add_trace(
-    go.Scatter(x = cpue_value_data['yearmonth'], y = cpue_value_data['avg_catch_value_usd']),
-    secondary_y = True
+    go.Scatter(
+        x = cpue_value_data['yearmonth'],
+        y = cpue_value_data['avg_catch_value_usd'],
+        name = 'Catch Value',
+        marker_color = '#f47762'
+    ), secondary_y = True
+)
+cpue_value_fig.update_layout(
+    title = 'Average CPUE per Trip and Average Catch Value',
+    xaxis_title = '',
+    yaxis = {
+        'title': 'CPUE (kg/trip)',
+        'titlefont': {
+            'color': '#5cb9ea'
+        },
+        'tickfont': {
+            'color': '#5cb9ea'
+        }
+    },
+    yaxis2 = {
+        'title': 'Catch Value (USD)',
+        'titlefont': {
+            'color': '#f47762'
+        },
+        'tickfont': {
+            'color': '#f47762'
+        }
+    }
 )
 cpue_value_plot = dcc.Graph(id = 'cpue-value-plot', figure = cpue_value_fig)
 
-# catch_plot_div = html.Div(
-#     [html.Label("Total Catch per Month (metric tons)"), catch_plot]
-# )
-# cpue_value_plot_div = html.Div(
-#     [html.Label("Average CPUE and average catch value"), cpue_value_plot]
-# )
-
-update_button = html.Button(children = 'Apply filters')
+update_button = html.Button(children = 'Apply filters', className = "btn btn-primary")
 
 plot_UI = html.Div([
     update_button,
     html.Br(),
+    html.Br(),
     catch_plot,
     html.Br(),
     cpue_value_plot
-], style = {'width': '49%', 'float': 'right', 'display': 'inline-block'})
+], style = {'width': '49%', 'float': 'right', 'display': 'inline-block', 'height': '600px', 'overflow-y': 'scroll'}
+)
